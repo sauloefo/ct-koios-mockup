ddf87fed4ff310c02b4b8cb9242479f70c996b3b
Saulo Oliveira <me@sauloefo.com>
2021-06-22 11:44:48 +0100

    wip

--- datastore.js
+++ datastore.js
@@ -1,7 +1,8 @@
 let Datastore = require('nedb');;
 
 if (!process.env.IS_PRODUCTION) {
-    Datastore = require('simple-odata-server-nedb');
+    console.log('Loading Datastore from local file system.');
+    Datastore = require('../simple-odata-server-nedb');
 }
 
 module.exports = Datastore;
--- odata-server.js
+++ odata-server.js
@@ -1,6 +1,7 @@
 let ODataServer = require('simple-odata-server');
 
 if (!process.env.IS_PRODUCTION) {
+    console.log('Loading odata-server from local file system.');
     ODataServer = require('../node-simple-odata-server');
 }
 
